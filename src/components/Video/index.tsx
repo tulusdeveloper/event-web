085e39e93f0e14034aeda81c59bfccda16f018c0
tulusdeveloper <tulusdeveloper@gmail.com>
2024-03-28 17:03:04 +0300

    video section

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -13,8 +13,8 @@ const Video = () => {
     <section className="relative z-10 py-16 md:py-20 lg:py-28">
       <div className="container">
         <SectionTitle
-          title="We are ready to help"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          title="Experience the Magic of Bliss & Blooms"
+          paragraph="Discover the art of event planning and design with our team of experts. Let us bring your vision to life and create an unforgettable experience."
           center
           mb="80px"
         />
