e40967e7a12663ece11d10c6ae4ff48df9085944
tulusdeveloper <tulusdeveloper@gmail.com>
2024-03-28 16:11:35 +0300

    Hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,26 +12,35 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Free and Open-Source Next.js Template for Startup & SaaS
+                  Welcome to Bliss n Blooms Events
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Startup is free Next.js template for startups and SaaS
-                  business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
-                  business website, built-with Next 13.x and Tailwind CSS.
+                  Bliss & Blooms is a premier event planning and design company
+                  based in Eldoret, Kenya. We specialize in weddings, corporate
+                  events, private parties, and more. Our team of experienced
+                  event planners and designers will work with you to create a
+                  truly unforgettable event that reflects your unique style and
+                  vision. We understand that every event is different, so we
+                  take the time to get to know you and your needs. We will work
+                  with you to create a custom event plan that is tailored to
+                  your budget and timeline. We are committed to providing our
+                  clients with the highest level of service and attention to
+                  detail. We believe that every event should be a celebration,
+                  and we are dedicated to making your event the best it can
+                  be.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nextjstemplates.com/templates/saas-starter-startup"
+                    href="/about"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                    🔥 Get Pro
+                    🔥Learn More
                   </Link>
                   <Link
-                    href="https://github.com/NextJSTemplates/startup-nextjs"
+                    href="/contact"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
-                    Star on GitHub
+                    Get in Touch
                   </Link>
                 </div>
               </div>
